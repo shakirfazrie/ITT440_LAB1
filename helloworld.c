8d092d9a29dffb62e35c97eec0f8971eac732aa3
shakirfazrie <shakirfazrie>
2021-04-17 14:20:22 +0000

    My first code

---
+++ helloworld.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (void){
+/*First C program*/
+
+printf("Hello World !");
+printf("I Love C");
+return(0);
+
+}
---
+++ userinput.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void){
+/*Second C Program*/
+
+int age;
+printf ("Hi, how old are you ?");
+scanf ("%d", &age);
+printf ("You are %d years old", age);
+return(0);
+
+}
